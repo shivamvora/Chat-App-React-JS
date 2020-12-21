feb5547d1e79bebff9d288e0a8ad485f26bcc740
shivam <shivampatel27102001@gmail.com>
2020-12-21 21:47:00 +0530

    again changed limit

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function SignOut() {
 function ChatRoom() {
   const dummy = useRef();
   const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(250);
+  const query = messagesRef.orderBy('createdAt').limit(25);
 
   const [messages] = useCollectionData(query, { idField: 'id' });
 
